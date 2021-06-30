f82e067f70e32f0dab67b46184037836a69fa39e
Donnahue George <Donnahuegjr@gmail.com>
2021-06-30 14:25:06 -0400

    added loop to check if item is in stock

--- script.py
+++ script.py
@@ -7,4 +7,17 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(PATH)
 driver.implicitly_wait(10)
-driver.get(page)
+
+while True:
+    driver.get(page)
+
+    button = driver.find_element_by_class_name("add-to-cart-button")
+
+    if button.text == "Sold Out":
+        print(button.text)
+        time.sleep(2)
+        driver.refresh()
+        continue
+    else:
+        print("write adding code")
+        break
