348711d118d2217619501735a55691b6ce3934e6
Donnahue George <Donnahuegjr@gmail.com>
2021-06-30 15:08:41 -0400

    got bot to craewl to customer information page

--- script.py
+++ script.py
@@ -16,6 +16,8 @@ button = "add-to-cart-button"
 cart = "cart-icon"
 shipping1 = '//*[@id="cartApp"]/div[2]/div[1]/div/div[1]/div[1]/section[1]/div[4]/ul/li/section/div[2]/div[2]/form/div[2]/fieldset/div[2]/div[1]/div/label/strong'
 shipping2 = '//*[@id="cartApp"]/div[2]/div[1]/div/div[1]/div[1]/section[2]/div/div/div[1]/form/ul/li[2]/div[1]/div/label/strong'
+checkout = "checkout-buttons__checkout"
+guest = "button-wrap"
 while True:
     if classname(button).text == "Sold Out":
         print(classname(button).text)
@@ -29,4 +31,7 @@ while True:
             xpath(shipping1).click()
         except NoSuchElementException:
             xpath(shipping2).click()
+        time.sleep(1)
+        classname(checkout).click()
+        classname(guest).click()
         break
