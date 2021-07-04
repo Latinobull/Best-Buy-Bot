5b24b226d0be99f862f0432099e9d9943338aba6
Donnahue George <Donnahuegjr@gmail.com>
2021-07-03 21:22:59 -0400

    getting all labels done

--- app.py
+++ app.py
@@ -7,15 +7,43 @@ window = Tk(
 
 def test():
     print(e1_value.get())
-    t1.delete("1.0", END)
-    t1.insert(END, e1_value.get())
 
 
-firstLabel = Label(window, text="Firstname")
+firstLabel = Label(window, text="Firstname:")
 firstLabel.grid(row=0, column=0)
 
-t1 = Text(window, height=1)
-t1.grid(row=0, column=2)
+lastLabel = Label(window, text="Lastname:")
+lastLabel.grid(row=1, column=0)
+
+addressLabel = Label(window, text="Address St(apt if necessary):")
+addressLabel.grid(row=2, column=0)
+
+cityLabel = Label(window, text="City:")
+cityLabel.grid(row=3, column=0)
+
+zipcodeLabel = Label(window, text="Zipcode:")
+zipcodeLabel.grid(row=4, column=0)
+
+emailLabel = Label(window, text="Email:")
+emailLabel.grid(row=5, column=0)
+
+phoneLabel = Label(window, text="Phone Number: (1)")
+phoneLabel.grid(row=6, column=0)
+
+cardLabel = Label(window, text="Card Number (no spaces):")
+cardLabel.grid(row=7, column=0)
+
+monthLabel = Label(window, text="Month of Expiration (MM):")
+monthLabel.grid(row=8, column=0)
+
+yearLabel = Label(window, text="Year of Expiration (YYYY):")
+yearLabel.grid(row=9, column=0)
+
+ccvLabel = Label(window, text="Security Code:")
+ccvLabel.grid(row=10, column=0)
+
+siteLabel = Label(window, text="Link to Website:")
+siteLabel.grid(row="11", column=0)
 
 e1_value = StringVar()
 e1 = Entry(window, width=20, textvariable=e1_value)
